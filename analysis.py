7c713b3416a3f6b8790e2eff9f4e60d53883a9f2
ArwaAwad-dotcom <aawad@mindsetsadvisors.com>
2025-02-03 14:43:42 +0200

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -32,6 +32,8 @@ def transform_date_column(df, column_name):
 
 
 
+    
+
 
 
 def get_relevant_data(file_name,level_one_list,level_two_list): 
@@ -131,16 +133,21 @@ def calculate_weights(all_data):
     
     for dd in all_data_levels_two:
         
+        #print(dd)
+        
         index_dd=all_data_levels_two.index(dd)
         
         data_sub=all_data[all_data['Level 2']==dd]
         for pos in list_dates_all_position:
+            #print(pos)
             index_pos=list_dates_all_position.index(pos)
             data_sub_two=data_sub[data_sub[pos].notna()]
             
             if len(data_sub_two)!=0:
                 list_weights= list(data_sub_two['Weight'])
+                #print("Weights",list_weights)
                 list_date_delivers=list(data_sub_two['Date of Reviewing'])
+                #print("Date of delivering",list_date_delivers)
                 list_grades=list(data_sub_two[pos])
                 combined = list(zip(list_date_delivers, list_weights, list_grades))
                 combined.sort(key=lambda x: x[0], reverse=True)
@@ -158,6 +165,7 @@ def calculate_weights(all_data):
                 sorted_weights=np.array(list(sorted_weights))
                 sorted_grades=np.array(list(sorted_grades))
                 grades_weigh=(np.dot(sorted_weights,sorted_grades))/(np.sum(sorted_weights))
+                #print(grades_weigh)
 
                 
                 if np.sum(sorted_weights) <=1 :
@@ -374,9 +382,229 @@ def calculate_psg_score_v2(weights,levels_one_two, all_psg_levels,file_psg):
     
     
     return final_psg_score
+
+
+
+
+
+    
+def level_one_aggregation(weights,levels_one_two, all_psg_levels,file_psg):
+    
+    
+        
+    #Read the matrix for each psg and level
+        
+    df_psg=pd.read_excel(file_psg)
+    
+    
+    column_two=list(df_psg.columns[1:])
+    
+    
+    matrix=df_psg.iloc[:,1:].to_numpy()
+      
+    
+    #Place the weights in a dataframe 
+    
+    psg_grade_data_frame=pd.DataFrame(weights)
+    
+    psg_grade_data_frame.columns=column_two
+    
+    psg_grade_data_frame['Level 2']=levels_one_two['Level 2']
+    
+    psg_grade_data_frame['Level 1']=levels_one_two['Level 1']
+    
+    col_all=list(levels_one_two.columns)
+    
+    col_all.extend(column_two)
+    
+    
+    psg_grade_data_frame=psg_grade_data_frame[col_all]
+    
+    
+    all_level_one=list(levels_one_two['Level 1'].unique())
+    
+
+    
+
+    #Start with the agregation
+
+    aggregate_level=np.zeros(matrix.shape)
+    
+    
+    #Get the top n per PSG and Level
+    
+    
+    for i,lev in enumerate(all_level_one):
+        for j,psg in enumerate(column_two):
+            
+            
+            df_sub=psg_grade_data_frame[psg_grade_data_frame['Level 1']==lev]
+            
+            
+            scores=np.array(list(df_sub[psg]))
+            scores =scores [~np.isnan(scores )]
+            avg_value=sum(scores)
+            
+            
+            aggregate_level[i,j]=avg_value
+    
+    
+    
+    aggregated_level_data_frame=pd.DataFrame(aggregate_level)
+    aggregated_level_data_frame.columns=column_two
+    aggregated_level_data_frame.index=all_level_one
+
+
+    
+    
+    return aggregated_level_data_frame
     
     
     
             
             
-            
+                
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Level 2 per PSG
+file_psg='PSG_Level_Matrix.xlsx'
+
+#Read all level one
+level_one=pd.read_excel('Level_one.xlsx')
+level_one_list=list(level_one['Level 1'])
+
+
+#Read all level 2
+level_two=pd.read_excel('Level_two.xlsx')
+level_two_list=list(level_two['Level 2'])
+
+levels_one_two=pd.DataFrame({'Level 1': level_one_list,'Level 2':level_two_list})
+
+
+
+
+file_name='Mindsets PMS_Engagement Assessment Form_Arwa_Kashmar_updated.xlsx'
+file_name_two='Mindsets PMS_Engagement Assessment Form_Arwa_Neom_updated.xlsx'
+ctf='Mindsets PMS_Comprehensive Talent Assessment Form_Arwa_v03.xlsx'
+baseline_data='Mindsets PMS_Baselining Sheet_v02.xlsx'
+#file_name='Lynn K Mindsets PMS_Engegement AF_Inv Manual & Proposal_MC_Final.xlsx'
+
+
+df=get_relevant_data(file_name,level_one_list,level_two_list)
+dg=get_relevant_data(file_name_two,level_one_list,level_two_list)
+all_data=pd.concat([df,dg])
+all_data_f=combine_baseline_data(baseline_data,all_data,level_one_list,level_two_list)
+
+columns_numeric=all_data_f.columns
+last_column=len(columns_numeric)-4
+columns_numeric=columns_numeric[2:last_column]
+
+for cl in columns_numeric:
+    all_data_f[cl]=pd.to_numeric(all_data_f[cl], errors='coerce')
+
+
+
+
+
+weights=calculate_weights(all_data_f)
+
+
+
+
+all_col=all_data_f.columns
+psg_levels=list(all_col[3:10])
+psg_final_grade=calculate_psg_score_v2(weights,levels_one_two,psg_levels,file_psg)
+psg_job_data=pd.read_excel('psg_job.xlsx')
+
+
+
+
+
+
+
+
+
+#Get the levels and psg level
+
+level_two_list=list(level_two['Level 2'])
+unique_engagement_number=list(all_data_f['Engagement Name'].unique())
+
+psg_levels='PSG 14'
+
+dict_engagement={}
+
+all_engagement={"Level 1":['Professional Behavior','Core Business Skills','Management Skills','Technical Knowledge']}
+
+unique_engagement_matrix=np.zeros((len(level_two_list),7))
+
+for un in unique_engagement_number:
+    print(un)
+    unique_data_frame=all_data_f[all_data_f['Engagement Name']==un]
+    unique_data_frame=unique_data_frame.iloc[:,:10]
+    
+    
+    list_grades=[]
+    for i in range(len(unique_data_frame)):
+        get_weight=unique_data_frame['Weight'].iloc[i]
+        list_grades_one=np.array(list(get_weight*unique_data_frame.iloc[i,3:]))
+        unique_engagement_matrix[i]= list_grades_one
+        
+    level_one_egagement_data_frame=level_one_aggregation(unique_engagement_matrix,levels_one_two,psg_levels,file_psg)
+    
+    list_used=list(level_one_egagement_data_frame[psg_levels])
+    
+    all_eng=list(all_engagement["Level 1"])
+    for ae in all_engagement["Level 1"]:
+        print(ae)
+        unique_data_frame_a=unique_data_frame[unique_data_frame["Level 1"]==ae]
+        list_weights=list(unique_data_frame_a['Weight'])
+        print(list_weights)
+        sum_weights=sum(list_weights)
+        if sum_weights>0: 
+            list_used[all_eng.index(ae)]=list_used[all_eng.index(ae)]/sum_weights
+    
+    all_engagement[un]=list_used
+        
+
+
+
+
+
+
+
+    dict_engagement[un]=level_one_egagement_data_frame
+    
+
+data_frame_averages=pd.DataFrame(all_engagement)
+
+
+    
+    
+
+    
+    
+
+
+    
+    
+    
+    
+
+    
